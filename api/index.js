b1d2a77e7a43750cc36712ec537feb3a818e6b95
pankajkdev <pankajk.dev0000@gmail.com>
2024-07-20 12:18:11 +0530

    change

--- api/index.js
+++
@@ -1,2 +0,0 @@
-import App from "../index";
-export default App;
--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-app.get("/api", (req, res) => {
-  res.send("<h1>Nah id win</h1>");
+app.get("/", (req, res) => {
+  res.send({
+    glossary: {
+      title: "example glossary",
+      GlossDiv: {
+        title: "S",
+        GlossList: {
+          GlossEntry: {
+            ID: "SGML",
+            SortAs: "SGML",
+            GlossTerm: "Standard Generalized Markup Language",
+            Acronym: "SGML",
+            Abbrev: "ISO 8879:1986",
+            GlossDef: {
+              para: "A meta-markup language, used to create markup languages such as DocBook.",
+              GlossSeeAlso: ["GML", "XML"],
+            },
+            GlossSee: "markup",
+          },
+        },
+      },
+    },
+  });
 });
 
 app.listen(process.env.PORT, () => {
   console.log(`http://localhost:${process.env.PORT}/`);
 });
+
+module.exports = app;
